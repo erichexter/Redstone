f6b2a65d4de417ade0843a034a3d3a6494a04994
Eric Hexter <eric.hexter@gmail.com>
2012-06-13 08:45:57 -0500

    updated nuspec to work on team city.

--- Mvc/ReadMe.txt
+++ Mvc/ReadMe.txt
@@ -1,6 +1,6 @@
-﻿MagLev - a solution template to build ASP.Net MVC applications quickly and simply.
+﻿Redstone - a solution template to build ASP.Net MVC applications quickly and simply.
 
-    MagLev provides a solution template which allows you to start developing features rather 
+    Redstone provides a solution template which allows you to start developing features rather 
 than setting up infrastructure like databases, unit testing, logging, error handling.
 
 This solution is opinionated! We value the SOLID object oriented design principles, 
@@ -8,4 +8,4 @@ which are: SRP, OCP, LSP, ICP, DIP http://en.wikipedia.org/wiki/SOLID_(object-or
 
 We also value limiting the number of abstractions in other words, we want to keep it cool and simple.
 
-For more information goto https://github.com/erichexter/MagLev
+For more information goto https://github.com/erichexter/RedStone
--- Solution/tools/init.ps1
+++ Solution/tools/init.ps1
@@ -1,45 +1,56 @@
-function global:install-maglev {
+function global:install-redstone {
 	$projectName = get-project | select-object -expandproperty ProjectName
+
+	"Setting Up UI"
+	install-package shortbus -IncludePrerelease
+	install-package structuremap
+	install-package StructureMap.MVC3
+	install-package WebActivator
+	install-package redstone.project.mvc -includePrerelease
+
+	"Setting Up Core"
 	add-project Core
 	add-projectReference $projectName Core
+	install-package shortbus.markers -projectname core -IncludePrerelease	
+
+	
 	add-project Data
 	add-projectReference Data Core
+	install-package -ProjectName Data EntityFramework
+	install-package -projectname Data shortbus.markers  -IncludePrerelease
+	install-package -projectname Data Newtonsoft.Json	  
+	install-package -projectName Data redstone.project.data -includeprerelease
+	Enable-Migrations -ProjectName Data
+
+	"Setting up Unit Tests"
+	"   Creating project and adding references"
 	add-project UnitTests
 	add-projectReference UnitTests $projectName
 	add-projectReference UnitTests Core
 	add-projectReference UnitTests Data
+
+	"   Installing Packages"
+	install-package -projectName UnitTests nunit 	
+	install-package -projectName UnitTests should 	
+	install-package -projectName UnitTests Moq 		
+	install-package -projectName UnitTEsts structuremap 	
+	install-package -projectName UnitTests redstone.project.unittests -includeprerelease
+	install-package -ProjectName UnitTests shortbus -IncludePrerelease
+
+	"Setting up Integration Tests"
+	"   Creating project and adding references"
 	add-project IntegrationTests
 	add-projectReference IntegrationTests $projectName
 	add-projectReference IntegrationTests Core
 	add-projectReference IntegrationTests Data
 
-	install-package shortbus -IncludePrerelease
-	install-package structuremap
-	install-package StructureMap.MVC3
-	install-package WebActivator
-	install-package maglev.project.mvc -includePrerelease
-
-	install-package EntityFramework -ProjectName Data
-	install-package shortbus.markers -projectname Data -IncludePrerelease
-	install-package Newtonsoft.Json	 -projectname Data 
-	install-package -projectName Data maglev.project.data -includeprerelease
-	Enable-Migrations -ProjectName Data
-
-	install-package shortbus.markers -projectname core -IncludePrerelease	
-
-	install-package nunit 		-projectName UnitTests
-	install-package should 		-projectName UnitTests
-	install-package Moq 		-projectName UnitTests
-	install-package structuremap 	-projectName UnitTEsts
-	install-package -projectName UnitTests maglev.project.unittests -includeprerelease
-	install-package -ProjectName UnitTests shortbus -IncludePrerelease
-
+	"   Installing Packages"
 	install-package -projectName IntegrationTests nunit
 	install-package -projectName IntegrationTests should
 	install-package -projectName IntegrationTests EntityFramework
 	install-package -projectName IntegrationTests EntityFramework.SqlServerCompact
 	install-package -projectName IntegrationTests SqlServerCompact
 	install-package -projectName IntegrationTests structuremap
-	install-package -projectName IntegrationTests maglev.project.integrationtests -includeprerelease
+	install-package -projectName IntegrationTests redstone.project.integrationtests -includeprerelease
 	install-package -ProjectName IntegrationTests shortbus -IncludePrerelease
 }
